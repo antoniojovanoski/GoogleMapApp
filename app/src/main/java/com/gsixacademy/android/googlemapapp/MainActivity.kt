563efb4bfe54416a5e7fe07071d6fe00e9250b03
antoniojovanoski <jovanoskiantonio1998@gmail.com>
2020-08-11 17:14:00 +0200

    aaa

--- app/src/main/java/com/gsixacademy/android/googlemapapp/MainActivity.kt
+++ app/src/main/java/com/gsixacademy/android/googlemapapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.gsixacademy.android.googlemapapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,7 +24,52 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        getLocationPermission()
+
     }
+
+    fun getLocationPermission(): Boolean {
+        val permissions = ArrayList<String>()
+        if (!isPermissionGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissions.add(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        if (permissions.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissions.toTypedArray(), 2000)
+            return false
+        } else
+            return true
+    }
+
+
+    private fun isPermissionGranted(permission: String): Boolean {
+        return this.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode==2002){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+               updateLocationUI()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationUI() {
+        if(isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            map.isMyLocationEnabled = true
+            map.uiSettings.isMyLocationButtonEnabled = true
+        }else{
+            map.isMyLocationEnabled = false
+            map.uiSettings.isMyLocationButtonEnabled = false
+            getLocationPermission()
+        }
+    }
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
@@ -41,6 +90,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             map.addMarker(MarkerOptions().position(dolnoSonjeLocation).title("Dolno Sonje"))
 //        map.animateCamera(CameraUpdateFactory.newLatLngZoom(gsixLocation,15f))
             map.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 90))
+            updateLocationUI()
         }
     }
 }
